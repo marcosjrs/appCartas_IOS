8244b8b73f791d20fe43422e3a73276c5a44166a
Marcos JRS <marcosjrs80@gmail.com>
2017-08-04 17:57:54 +0200

    Initial Commit

---
+++ appCartas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  appCartas
+//
+//  Created by Marcos JRS on 4/8/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appCartas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  appCartas
+//
+//  Created by Marcos JRS on 4/8/17.
+//  Copyright © 2017 Marcos JRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
