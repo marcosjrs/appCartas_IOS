c3d46992ad28ee234420e23dae5e3e6dd037d73a
Marcos JRS <marcosjrs80@gmail.com>
2017-08-05 10:05:37 +0200

    Implementación de "Número de Giros" de la carta

--- appCartas/ViewController.m
+++ appCartas/ViewController.m
@@ -9,10 +9,18 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount; //Numero de vueltas. Utiliza propiedad, para utilizar el setter para modificar el label...
 
 @end
 
 @implementation ViewController
+
+
+-(void) setFlipCount:(int)flipCount{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Giros: %d",self.flipCount]; //utilizamos el getter, por si en algún momento se quisiera controlar...
+}
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     //La idea es el efecto de darle la vuelta cada vez que se toque. Por un lado tendrá el texto de la carta y por el otro no tendrá texto y tendrá el logo. Por tanto para saber si está dado la vuelta podemos mirar si tiene texto o no
@@ -29,6 +37,8 @@
         [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
         
     }
+    
+    self.flipCount++; //utilizando el setter
 }
 
 
